c80be93f94f8399bed7a706e8dcdcf7737a646a0
Hugo <usava147@gmail.com>
2026-03-02 18:25:38 +0100

    Add files via upload

--- tornei/logiche/logica_draft12_DxSx.py
+++ tornei/logiche/logica_draft12_DxSx.py
@@ -1,13 +1,8 @@
-# logiche/logica_draft12.py
-from .logica_draft_generic import generate_ds_rounds, build_schedule_from_rounds
+# from .logica_draft_generic import generate_ds_rounds, build_schedule_from_rounds
+from .logica_draft_generic import generate_ds_rounds, build_schedule_with_backtracking
 
 def solve_draft12(destri, sinistri):
-    """
-    destri: list di 6 nomi
-    sinistri: list di 6 nomi
-    ritorna: pd.DataFrame con Turno, Campo, Coppia A, Coppia B (6 turni)
-    """
     if len(destri) != 6 or len(sinistri) != 6:
         raise ValueError("solve_draft12 richiede 6 destri e 6 sinistri")
     base_rounds = generate_ds_rounds(destri, sinistri)
-    return build_schedule_from_rounds(base_rounds)
+    return build_schedule_with_backtracking(base_rounds, set(destri))
